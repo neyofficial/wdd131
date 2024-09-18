a762f4f95c585bf02f75bc3d421ed30429dd4c27
Nana Esi <almighty.ney@gmail.com>
2024-09-18 10:03:05 +0000

    webpage code dev

---
+++ week01/scripts/getdates.js
@@ -0,0 +1,8 @@
+// Get current year and display in footer
+const yearElement = document.getElementById("currentyear");
+const currentYear = new Date().getFullYear();
+yearElement.textContent = currentYear;
+
+// Display last modified date in footer
+const lastModifiedElement = document.getElementById("lastModified");
+lastModifiedElement.textContent = `Last Modified: ${document.lastModified}`;
